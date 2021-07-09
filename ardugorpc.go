1b4955ff295e92b16b7530f6ee418be09a81ac20
Asset Malik <asetmalik@gmail.com>
2021-07-09 17:26:11 +0600

    ADD: assert_version

--- ardugorpc.go
+++ ardugorpc.go
@@ -1,5 +1,7 @@
 package ardugorpc
 
+import "fmt"
+
 const _protocol = "simpleRPC"
 
 var _version = [3]int{3, 0, 0}
@@ -7,5 +9,11 @@ var _version = [3]int{3, 0, 0}
 func _assert_protocol(protocol string) {
 	if protocol != _protocol {
 		panic("invalid protocol header")
+	}
+}
+
+func _assert_version(version []int) {
+	if version[0] != _version[0] || version[1] > _version[1] {
+		panic(fmt.Sprintf("version mismatch (device: %v, client: %v)", version, _version))
 	}
 }
